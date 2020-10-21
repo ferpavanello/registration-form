f1a4c632ae31efc48dffda2eed964097db5ff8ed
Fernando Pavanello <fernando.pavanello2@gmail.com>
2020-10-21 06:38:20 -0300

    Add style from material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import "fontsource-roboto";
 import RegistrationForm from "./components/registration-form/RegistrationForm";
+import { Container, Typography } from "@material-ui/core";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <h1>Registration Form</h1>
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center">
+          Registration Form
+        </Typography>
         <RegistrationForm />
-      </React.Fragment>
+      </Container>
     );
   }
 }
--- src/components/registration-form/RegistrationForm.jsx
+++ src/components/registration-form/RegistrationForm.jsx
@@ -1,24 +1,38 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function RegistrationForm() {
   return (
     <form>
-      <label>Name</label>
-      <input type="text" />
-
-      <label>Last Name</label>
-      <input type="text" />
-
-      <label>CPF</label>
-      <input type="text" />
-
-      <label>Promotions</label>
-      <input type="checkbox" />
-
-      <label>Newsletter</label>
-      <input type="checkbox" />
-
+      <TextField
+        id="name"
+        label="Name"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="lastname"
+        label="Last Name"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <FormControlLabel
+        label="Promotions"
+        control={<Switch name="promotions" defaultChecked color="primary" />}
+      />
+      <FormControlLabel
+        label="Newsletter"
+        control={<Switch name="newsletter" defaultChecked color="primary" />}
+      />
       <Button type="submit" variant="contained" color="primary">
         Submit
       </Button>
