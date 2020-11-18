9cf722a12ef331c7e74d6039eb745ca98ef07f22
Fernando Pavanello <fernando.pavanello2@gmail.com>
2020-11-18 06:42:21 -0300

    Concat data from different forms using useEffect

--- src/components/registration-form/RegistrationForm.jsx
+++ src/components/registration-form/RegistrationForm.jsx
@@ -1,21 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import DeliveryInfo from './DeliveryInfo'
 import PersonalInfo from './PersonalInfo'
 import UserInfo from './UserInfo'
+import { Step, Stepper, StepLabel, Typography } from '@material-ui/core'
 
 function RegistrationForm({ formSubmit, validateCpf }) {
   const [actualStage, setActualStage] = useState(0)
+  const [formData, setFormData] = useState({})
   const forms = [
-    <UserInfo formSubmit={nextStage} />,
-    <PersonalInfo formSubmit={nextStage} validateCpf={validateCpf} />,
-    <DeliveryInfo formSubmit={formSubmit} />
+    <UserInfo formSubmit={concatData} />,
+    <PersonalInfo formSubmit={concatData} validateCpf={validateCpf} />,
+    <DeliveryInfo formSubmit={concatData} />,
+    <Typography>Thanks for the registration</Typography>,
   ]
+
+  useEffect(() => {
+    if (actualStage === forms.length - 1) {
+      formSubmit()
+    }
+  })
+
+  function concatData(newData) {
+    setFormData({ ...formData, ...newData })
+    nextStage()
+  }
 
   function nextStage() {
     return setActualStage(actualStage + 1)
   }
 
-  return <>{forms[actualStage]}</>
+  return (
+    <>
+      <Stepper activeStep={actualStage}>
+        <Step>
+          <StepLabel>Login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Personal Info</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Delivery</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>End</StepLabel>
+        </Step>
+      </Stepper>
+      {forms[actualStage]}
+    </>
+  )
 }
 
 export default RegistrationForm
